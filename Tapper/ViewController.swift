d64989bcf92a54d65c7f6ed968e0c84c807fb9fd
Joseph Pilon <jpilon@gulptech.com>
2016-03-18 22:28:36 -0400

    Finished Taper Exercise

--- Tapper/ViewController.swift
+++ Tapper/ViewController.swift
@@ -8,18 +8,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var tapperImg: UIImageView!
+    @IBOutlet weak var coinBtn: UIButton!
+    @IBOutlet weak var tapLbl: UILabel!
+    @IBOutlet weak var tapNumberTextField: UITextField!
+    @IBOutlet weak var playImg: UIImageView!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    var maxTaps = 0
+    var curTaps = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tapNumberTextField.delegate = self
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func playTapped(sender: AnyObject) {
+        
+        if let taps = tapNumberTextField.text where !taps.isEmpty {
+            maxTaps = Int(taps)!
+            tapperImg.hidden = true
+            coinBtn.hidden = false
+            tapLbl.hidden = false
+            tapNumberTextField.hidden = true
+            playImg.hidden = true
+            playBtn.hidden = true
+        }
+    }
+    @IBAction func coinTapped(sender: AnyObject) {
+        if curTaps > maxTaps  {
+            tapLbl.text = "Tap the coin to reset"
+            curTaps = 0
+        } else if curTaps == 0 {
+            curTaps = 1
+            maxTaps = 0
+            tapperImg.hidden = false
+            coinBtn.hidden = true
+            tapLbl.hidden = true
+            tapNumberTextField.text = ""
+            tapNumberTextField.hidden = false
+            playImg.hidden = false
+            playBtn.hidden = false
+        } else {
+            if curTaps == 1 {
+                tapLbl.text = "\(curTaps) Tap"
+            } else {
+                tapLbl.text = "\(curTaps) Taps"
+            }
+            curTaps += 1
+        }
     }
 
-
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let allowedCharacters = NSCharacterSet(charactersInString: "1234567890")
+        let replacementTextIsNumeric = string.rangeOfCharacterFromSet(allowedCharacters)
+        
+        if replacementTextIsNumeric == nil {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
 }
 
